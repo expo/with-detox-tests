bc58a7cbf2a4e5328b58489867300ba48ce19f23
Patrick Curry <patrick.curry@countingup.com>
2018-10-24 12:14:37 +0100

    Updated detox, expo and react-native, using jest instead of mocha
    See comments in e2e/firstTest.spec.js

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,7 +1,40 @@
-const { reloadApp } = require('detox-expo-helpers');
+const { getAppUrl, blacklistLiveReloadUrl } = require('detox-expo-helpers');
+
+// Copied from detox-expo-helpers so we can disable sync before blacklisting.
+const reloadApp = async (params) => {
+  const url = await getAppUrl();
+  await device.launchApp({
+    permissions: params && params.permissions,
+    newInstance: true,
+    url,
+    sourceApp: 'host.exp.exponent',
+    // Setting this to false will show Expo's intro page on each restart.
+    // This has the "Disable Live Reload" button, which can be tapped with Detox:
+    // `await element(by.text("Disable Live Reload")).tap();`
+    // I've had some luck with other projects where this removes the need to disable sync. Not the case with this project for some reason.
+    launchArgs: { EXKernelDisableNuxDefaultsKey: true },
+  });
+
+  // Should not have to do this!
+  // Detox/EarlGrey synchronisation does not play well with Expo.
+  await device.disableSynchronization();
+
+  // This also doesn't seem to work. Apparently it used to.
+  // This should allow us to use synchronisation as it will ignore events such as Expo's live reload polling.
+  await blacklistLiveReloadUrl();
+};
 
 describe('Example', () => {
   beforeEach(async () => {
+    // Should not have to do this!
+    // Detox/EarlGrey synchronisation does not play well with Expo.
+    // This can be turned off (see above) but is reset on app restart.
+    // There seems to be no way to disable sync before we're already blocked on something else.
+    // So instead, wait some time and hope the app has started.
+    device.deviceDriver.waitUntilReady = async () => {
+      await new Promise(done => setTimeout(done, 5000)) // wait 5 seconds
+    };
+
     await reloadApp();
   });
 
